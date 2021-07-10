ec1ae8db4397e5a09c26263519ad75293be6bd94
Arnas Savickas <arnas.savi@gmail.com>
2021-07-10 22:31:30 +0300

    Finish 04

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,23 +5,40 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
+
+const userBuilder = optionalData => {
+  const userData = build('User', {
+    fields: {
+      username: fake(f => f.internet.userName()),
+      password: fake(f => f.internet.password()),
+      ...optionalData,
+    },
+  })
+  return userData()
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  const handleSubmit = jest.fn()
+
+  render(<Login onSubmit={handleSubmit} />)
+
+  const usernameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
+
+  const {username, password} = userBuilder({password: 'abc'})
+
+  const inputData = {
+    username,
+    password,
+  }
+
+  userEvent.type(usernameInput, inputData.username)
+  userEvent.type(passwordInput, inputData.password)
+  userEvent.click(screen.getByText(/submit/i))
+
+  expect(handleSubmit).toHaveBeenCalledWith(inputData)
 })
 
 /*
